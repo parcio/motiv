269ec007e9935b58feeb40b8c60fe574a0d703e2
Korsar3435 <t.a.cirkov@gmail.com>
2024-02-04 20:09:56 +0100

    test-3

--- src/models/ViewSettings.hpp
+++ src/models/ViewSettings.hpp
@@ -153,14 +153,14 @@ private:
     QString executablePath = QCoreApplication::applicationDirPath();
 
     std::map<std::string, QIcon> Icons_ {
-        {std::pair<std::string, QIcon>{"plus", executablePath.chopped(6) + ":/res/tango_icons_png/plus.png"}},
+        {std::pair<std::string, QIcon>{"plus", executablePath.chopped(6) + "/res/tango_icons_png/plus.png"}},
         {std::pair<std::string, QIcon>{"plus_grey", executablePath.chopped(6) + "/res/tango_icons_png/plus_grey.png"}},
         {std::pair<std::string, QIcon>{"minus", executablePath.chopped(6) + "/res/tango_icons_png/minus.png"}},
         {std::pair<std::string, QIcon>{"zoom_in", executablePath.chopped(6) + "/res/tango_icons_png/zoom_in.png"}},
         {std::pair<std::string, QIcon>{"zoom_out", executablePath.chopped(6) + "/res/tango_icons_png/zoom_out.png"}},
         {std::pair<std::string, QIcon>{"zoom_fit", executablePath.chopped(6) + "/res/tango_icons_png/zoom_fit.png"}},
         {std::pair<std::string, QIcon>{"search", executablePath.chopped(6)+ "/res/tango_icons_png/search.png"}},
-        {std::pair<std::string, QIcon>{"book", executablePath.chopped(6) + "/res/tango_icons_png/book.png"}},
+        {std::pair<std::string, QIcon>{"book", executablePath.chopped(6) + ":/res/tango_icons_png/book.png"}},
         {std::pair<std::string, QIcon>{"refresh", executablePath.chopped(6) + "/res/tango_icons_png/refresh.png"}},
         {std::pair<std::string, QIcon>{"maximize", executablePath.chopped(6) + "/res/tango_icons_png/maximize.png"}}
     };
